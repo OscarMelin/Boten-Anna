8d45fcec58cf68a6084fd1acf7a40dc19bf1f5db
Oscar <oscar.melin@kaplan.se>
2015-06-19 16:26:30 +0200

    Move api calls to api.py module

--- api.py
+++ api.py
@@ -1,2 +1,33 @@
-def test():
-    print("jag funkar")
+import json
+import urllib.request
+
+
+API_URL = "https://slack.com/api/"
+
+with open("config.json") as config_file:
+    settings = json.load(config_file)
+    USERTOKENSTRING = settings["USERTOKENSTRING"]
+    URLTOKENSTRING = settings["URLTOKENSTRING"]
+    TEAMNAMESTRING = settings["TEAMNAMESTRING"]
+
+def get_users():
+    with urllib.request.urlopen("{0}users.list?token={1}".format(API_URL, USERTOKENSTRING)) as response:
+        return json.loads(response.read().decode("utf-8"))["members"]
+
+
+def get_channels():
+    with urllib.request.urlopen("{0}channels.list?token={1}".format(API_URL, USERTOKENSTRING)) as response:
+        return json.loads(response.read().decode("utf-8"))["channels"]
+
+def post_message(message):
+    #channel "test" id:C06E3DG6S
+    with urllib.request.urlopen("{0}chat.postMessage?token={1}&channel=C06E3DG6S&text={2}&username=Boten%20Anna".format( \
+        API_URL, USERTOKENSTRING, message)) as response:
+        return response.read().decode("utf-8")
+
+def get_latest_messages(amount):
+    #channel "test" id:C06E3DG6S
+    with urllib.request.urlopen("{0}channels.history?token={1}&channel=C06E3DG6S&count={2}".format( \
+        API_URL, USERTOKENSTRING, str(amount))) as response:
+        return json.loads(response.read().decode("utf-8"))["messages"]
+
--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-import urllib.request
-import json
 import time
-import funcs
+import api
 import random
-
-API_URL = "https://slack.com/api/"
-
-with open("config.json") as config_file:
-    settings = json.load(config_file)
-    USERTOKENSTRING = settings["USERTOKENSTRING"]
-    URLTOKENSTRING = settings["URLTOKENSTRING"]
-    TEAMNAMESTRING = settings["TEAMNAMESTRING"]
 
 
 class User:
@@ -20,75 +10,70 @@ class User:
 
 def load_all_users():
     all_users = dict()
-    for user in get_users():
+    for user in api.get_users():
         user = User(user["name"], user["id"])
         all_users[user.id] = user
     return all_users        
 
-def get_users():
-    with urllib.request.urlopen("{0}users.list?token={1}".format(API_URL, USERTOKENSTRING)) as response:
-        return json.loads(response.read().decode("utf-8"))["members"]
-
-
-def get_channels():
-    with urllib.request.urlopen("{0}channels.list?token={1}".format(API_URL, USERTOKENSTRING)) as response:
-        return json.loads(response.read().decode("utf-8"))["channels"]
-
-def post_message(message):
-    #channel "test" id:C06E3DG6S
-    with urllib.request.urlopen("{0}chat.postMessage?token={1}&channel=C06E3DG6S&text={2}&username=Boten%20Anna".format( \
-        API_URL, USERTOKENSTRING, message)) as response:
-        return response.read().decode("utf-8")
-
-def get_latest_messages(amount):
-    #channel "test" id:C06E3DG6S
-    with urllib.request.urlopen("{0}channels.history?token={1}&channel=C06E3DG6S&count={2}".format( \
-        API_URL, USERTOKENSTRING, str(amount))) as response:
-        return json.loads(response.read().decode("utf-8"))["messages"]
-
 def start_listener(started, users, latest_messages):
+    """
+    First stage.
+    """
     if not started:
         for message in latest_messages:
             if message["text"] == "start":
-                post_message("Started%20by%20" + users[message["user"]].name)
+                api.post_message("Started%20by%20" + users[message["user"]].name)
                 return True
     return False
+
+def register():
+    """
+    Second stage.
+    """
+    pass
+
+def choose_loser():
+    """
+    Last stage.
+    """
+    pass
 
 def main():
 
     started = False
     ended = False
-    post_message("New%20session%20active.")
-    post_message("Type%20'start'%20to%20start.")
+    api.post_message("New%20session%20active.")
+    api.post_message("Type%20'start'%20to%20start.")
 
     while True:
         
-        latest_messages = get_latest_messages(5)
+        latest_messages = api.get_latest_messages(5)
         users = load_all_users()
         started = start_listener(started, users, latest_messages)
 
         if started:
-            
-            post_message("Started.%20Type%20'register'%20to%20register")
+            started_message = "Started.%20Type%20'register'%20to%20register"
+            api.post_message(started_message)
             time.sleep(20)
             participants = []
-            for message in get_latest_messages(10):                
-
+            for message in api.get_latest_messages(10):                
                 if message["text"] == "register":
-                    post_message(users[message["user"]].name + "%20registered")
+                    api.post_message(users[message["user"]].name + "%20registered")
                     participants.append(users[message["user"]])
                     ended = True
-                elif message["text"] == "Started.%20Type%20'register'%20to%20register":
+                elif message["text"] == started_message.replace("%20", " "):
                     break
                     
         if ended:
             
-            post_message("Registered%20participants:")
+            api.post_message("Registered%20participants:")
+            #Remove duplicate registrations
             participants = list(set(participants))
             for user in participants:
-                post_message(user.name)
-                
-            post_message("Session%20ended")
+                api.post_message(user.name)
+
+            api.post_message("Loser:%20{0}".format(random.choice(participants).name))
+            api.post_message("Session%20ended")
             break
         
         else:        
