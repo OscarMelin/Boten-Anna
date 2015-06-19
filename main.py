46a4841d8b0c80571e78f90201f36b6b87b6e5aa
Oscar <oscar.melin@kaplan.se>
2015-06-19 16:47:36 +0200

    split main() into three smaller functions

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class User:
 
 def load_all_users():
     all_users = dict()
+    
     for user in api.get_users():
         user = User(user["name"], user["id"])
         all_users[user.id] = user
@@ -26,22 +27,45 @@ def start_listener(started, users, latest_messages):
                 return True
     return False
 
-def register():
+def register(seconds, all_users):
     """
     Second stage.
+    Hold registration open for seconds amount of time.
     """
-    pass
+    participants = []
+    started_message = "Started.%20Type%20'register'%20to%20register"
+    
+    api.post_message(started_message)
+    time.sleep(seconds)
+    
+    for message in api.get_latest_messages(30):                
+        if message["text"] == "register":
+            api.post_message(all_users[message["user"]].name + "%20registered")
+            participants.append(all_users[message["user"]])
+            ended = True
+        elif message["text"] == started_message.replace("%20", " "):
+            break
+    return participants
 
-def choose_loser():
+def choose_loser(participants):
     """
     Last stage.
     """
-    pass
+    #Remove duplicate registrations
+    participants = list(set(participants))
+    
+    api.post_message("Registered%20participants:")
+    for user in participants:
+        api.post_message(user.name)
+
+    api.post_message("Loser:%20{0}".format(random.choice(participants).name))
+    api.post_message("Session%20ended")
 
 def main():
-
+    """
+    
+    """
     started = False
-    ended = False
     api.post_message("New%20session%20active.")
     api.post_message("Type%20'start'%20to%20start.")
 
@@ -50,47 +74,18 @@ def main():
         latest_messages = api.get_latest_messages(5)
         users = load_all_users()
         started = start_listener(started, users, latest_messages)
+        participants = []
 
         if started:
-            started_message = "Started.%20Type%20'register'%20to%20register"
-            api.post_message(started_message)
-            time.sleep(20)
-            participants = []
-            for message in api.get_latest_messages(10):                
-                if message["text"] == "register":
-                    api.post_message(users[message["user"]].name + "%20registered")
-                    participants.append(users[message["user"]])
-                    ended = True
-                elif message["text"] == started_message.replace("%20", " "):
-                    break
+            participants = register(20, users)
                     
-        if ended:
-            
-            api.post_message("Registered%20participants:")
-            #Remove duplicate registrations
-            participants = list(set(participants))
-            for user in participants:
-                api.post_message(user.name)
-
-            api.post_message("Loser:%20{0}".format(random.choice(participants).name))
-            api.post_message("Session%20ended")
+        if participants:
+            choose_loser(participants)
             break
         
         else:        
             continue          
         time.sleep(2)
-        
-    
+            
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
-
-
-    
